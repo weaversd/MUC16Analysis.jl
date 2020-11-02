432a9d363cc4f8bcec1e9cf240a9be91a39ced14
weaversd <69007980+weaversd@users.noreply.github.com>
2020-11-02 12:21:07 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,6 @@ using Test
 @testset "MUC16Analysis.jl" begin
     @test 10 == 10 #just to make sure test is working
     @test letter_list(5) == ["A", "B", "C", "D", "E"]
+    SUBMAT_ALI = sub_mat_ali()
+    @test SUBMAT_ALI[AA_A,AA_R] === 90
 end
