1d3759cbacd538ecd5f6d52d9d661224c70ab77e
weaversd <69007980+weaversd@users.noreply.github.com>
2020-11-02 08:52:55 -0500

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using MUC16Analysis
 using Test
 
 @testset "MUC16Analysis.jl" begin
-    @test 10 == 10
-    @test 6+4 == 10
-    @test 6+4 == 9
+    @test 10 == 10 #just to make sure test is working
+    @test letter_list(5) == ["A", "B", "C", "D", "E"]
 end
