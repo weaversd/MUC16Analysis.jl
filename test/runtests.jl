ef52f3dfec35cc9e0432a8efa479d8e46b0d20f6
weaversd <69007980+weaversd@users.noreply.github.com>
2020-11-02 12:29:39 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,18 @@
 using MUC16Analysis
 using BioSequences
+using DataFrames
 using Test
 
 @testset "MUC16Analysis.jl" begin
     @test 10 == 10 #just to make sure test is working
-    @test letter_list(5) == ["A", "B", "C", "D", "E"]
-    SUBMAT_ALI = sub_mat_ali()
-    @test SUBMAT_ALI[AA_A,AA_R] === 90
+    
+    @test letter_list(5) == ["A", "B", "C", "D", "E"] 
+    
+    SUBMAT_ALI = sub_mat_ali() #create the ali et al substitution matrix
+    @test SUBMAT_ALI[AA_A,AA_R] === 90 #check random values in the substitution matrix
+    @test SUBMAT_ALI[AA_G,AA_D] === 65
+    @test SUBMAT_ALI[AA_P,AA_P] === 0
+    
+    example_dmdf = CSV.readjoinpath(dirname(@__FILE__), "data_files", "example.csv")
+    println(example_dmdf)
 end
