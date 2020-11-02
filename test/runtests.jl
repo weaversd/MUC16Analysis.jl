9711fc3b2384170ff38450c553205f8413e97934
weaversd <69007980+weaversd@users.noreply.github.com>
2020-11-02 12:12:15 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 @testset "MUC16Analysis.jl" begin
     @test 10 == 10 #just to make sure test is working
     @test letter_list(5) == ["A", "B", "C", "D", "E"]
+    @test typeof(sub_mat_ali()) == BioAlignments.SubstitutionMatrix{BioSymbols.AminoAcid,Int64}
 end
