795b50e01602cd987f9bf147b8e71f7d5b74f3a6
weaversd <69007980+weaversd@users.noreply.github.com>
2020-11-02 13:55:14 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,6 +30,6 @@ using CSV
     example_dm_fromfasta = dist_mat_gen(joinpath(dirname(@__FILE__), "example.fasta"), "example_dm_fromfasta.csv")
     @test nrow(example_dm_fromfasta) == 10
     add_missing(example_dm_fromfasta)
-    example_newick3 = upgma(example_dm_fromfasta, output = "example3.tree)
+    example_newick3 = upgma(example_dm_fromfasta, output = "example3.tree")
     @test example_newick3 == "((((1:2068.0,6:2068.0):138.70833333333348,(((2:1948.5,4:1948.5):155.0,10:2103.5):66.77777777777783,(3:2102.5,(5:2001.0,8:2001.0):101.5):67.77777777777783):36.43055555555566):12.229166666666515,7:2218.9375):63.17361111111131,9:2282.1111111111113);"
 end
